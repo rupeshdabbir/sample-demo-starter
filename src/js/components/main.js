0d1dd6195f1038ae8d7726f3716702be4fd53fc7
Rupesh Dabbir <d.rupeshkumar@gmail.com>
2019-08-29 10:30:09 -0700

    Sample

---
+++ src/js/components/main.js
@@ -0,0 +1,11 @@
+// This is an examples of simple export.
+//
+// You can remove or add your own function in this file.
+
+const getHello = () => {
+  return {
+    'hello': 'world'
+  }
+};
+
+export default getHello;
